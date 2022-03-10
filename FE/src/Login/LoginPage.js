8ca51ac4bbc7bf79e8b1d6d5bacbc2c2de63e1ce
Ivan <ivanteozhiming@gmail.com>
2022-03-10 19:06:18 +0800

    fix fe redirect after create user

--- FE/src/Login/LoginPage.js
+++ FE/src/Login/LoginPage.js
@@ -151,33 +151,29 @@ export default function LoginPage() {
       localStorage.setItem("userID", createUserData.userID);
       localStorage.setItem("userName", createUserData.name);
       //redirect to the loan page once done
-      const path = Routes.loan.loan+params.id;
-      console.log("path: ", path)
+      const path = `/${Routes.loan.loan}${params.id}`;
       navigate(path);
     }
   }, [createUserData]);
 
   const isPageOne = () => {
-    // return signInData === null;
     return signInData === null;
-    // return false
   };
 
   const isPageTwo = () => {
     // Don't remove the comment below -- actual logic when no longer testing
-    // return signInData && verifyOTPData === null;
-    // return false
+    return signInData && verifyOTPData === null;
+
     // Remove this when no longer testing
-    return signInData !== null && pageState !== 3;
+    // return signInData !== null && pageState !== 3;
   };
 
   const isPageThree = () => {
     // Don't remove the comment below -- actual logic when no longer testing
-    // return verifyOTPData && createUserData === null;
-    // return true
+    return verifyOTPData && createUserData === null;
 
     // Remove this when no longer testing
-    return pageState === 3;
+    // return pageState === 3;
   };
 
   return (
